2baedc30cf2081a1eb3bbd81a03198cf9df0d481
ropinrocha <romulo_pbr@hotmail.com>
2023-08-13 00:59:16 -0300

    Conversor de Segundos de Fato!
    Um conversor de segundos onde o usuário digita o valor desejado em segundos e então é mostrado na tela no formato hh:mm:ss (hh - horas;mm - minutos;ss - segundos).

--- ConversorDeSegundos/ConversorDeSegundos.cs
+++ ConversorDeSegundos/ConversorDeSegundos.cs
@@ -6,8 +6,17 @@ namespace ConversorDeSegundos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int SecOutPut = int.Parse(Console.ReadLine());
+            int Hour, min , sec;
+
+            Hour = SecOutPut / 3600;
+            min = SecOutPut % 3600 / 60;
+            sec = SecOutPut % 60;
+
+            Console.WriteLine($"{Hour}:{min}:{sec}");
             Console.ReadKey();
+
+
         }
     }
 }
